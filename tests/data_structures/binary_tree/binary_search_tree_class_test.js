c534706cee80a875d8b640bf9ef04ca7e826e782
Sergei <sselihov@brightsign.biz>
2023-06-10 19:06:53 +0300

    Add some tests for binary search tree.

---
+++ tests/data_structures/binary_tree/binary_search_tree_class_test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// external imports
+import test from 'node:test';
+
+// internal imports
+import { convertArrayToTree, checkTree } from './../../../src/utils/testing/data_structures/binary_tree/binary_search_tree_class_test_utils.js';
+
+// implementation
+test('BinarySearchTreeClass tests...', async (t) => {
+    await t.test('Instance creation tests...', async (t) => {
+        const keys1 = [8, 3, 10, 1, 6, 4, 7, 14, 13];
+        const keys2 = [11, 7, 15, 5, 3, 9, 8, 10, 13, 12, 14, 20, 18, 25];
+
+        const testSequence1 = [8, 3, 10, 1, 6, null, 14, null, null, 4, 7, null, null, 13, null, null, null];
+        const testSequence2 = [11, 7, 15, 5, 9, 13, 20, 3, null, 8, 10, 12, 14, 18, 25];
+
+        await t.test('Should create a BinarySearchTreeNode - case 1', () => {
+            const treeInstance = convertArrayToTree(keys1);
+            checkTree(treeInstance, testSequence1);
+        });
+
+        await t.test('Should create a BinarySearchTreeNode - case 2', () => {
+            const treeInstance = convertArrayToTree(keys2);
+            checkTree(treeInstance, testSequence2);
+        });
+    });
+});
+
+// exports
