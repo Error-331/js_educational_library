a58c465d4c0c4d8df1749d98c65cdc7329a6a07e
Sergei <sselihov@brightsign.biz>
2023-06-10 19:07:27 +0300

    Add binary search tree class.

---
+++ src/data_structures/binary_tree/binary_search_tree_class.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// external imports
+
+// internal imports
+import { COMPARATOR_LESS_THAN } from './../../constants/comparator_constants.js';
+
+import { isNil } from './../../utils/misc/logic_utils.js';
+import { defaultCompare } from './../../utils/misc/comparator_utils.js';
+
+import BinarySearchTreeNodeClass from './binary_search_tree_node_class.js';
+
+// implementation
+class BinarySearchTreeClass {
+    #root = null;
+    #comparator = defaultCompare;
+
+    #insertNode(parent, key) {
+        if (this.#comparator(key, parent.key) === COMPARATOR_LESS_THAN) {
+            if (parent.left === null) {
+                parent.left = new BinarySearchTreeNodeClass(key, parent);
+            } else {
+                this.#insertNode(parent.left, key);
+            }
+        } else {
+            if (parent.right === null) {
+                parent.right = new BinarySearchTreeNodeClass(key, parent);
+            } else {
+                this.#insertNode(parent.right, key);
+            }
+        }
+    }
+
+    insert(key) {
+        if (this.#root === null) {
+            this.#root = new BinarySearchTreeNodeClass(key);
+        } else {
+            this.#insertNode(this.#root, key);
+        }
+    }
+
+    get root() {
+        return this.#root;
+    }
+
+    constructor(comparator) {
+        if (!isNil(comparator)) {
+            this.#comparator = comparator;
+        }
+    }
+}
+
+// exports
+export default BinarySearchTreeClass;
