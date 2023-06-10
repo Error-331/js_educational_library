12f11acbdb672ed324ba9ccb93175fbe5caa48c2
Sergei <sselihov@brightsign.biz>
2023-06-10 14:58:25 +0300

    Minor fixes.

--- src/constants/comparator_constants.js
+++ src/constants/comparator_constants.js
@@ -1,9 +1,15 @@
 'use strict';
 
+// external imports
+
+// internal imports
+
+// implementation
 const COMPARATOR_LESS_THAN = -1;
 const COMPARATOR_GREATER_THAN = 1;
 const COMPARATOR_EQUAL = 0;
 
+// exports
 export {
     COMPARATOR_LESS_THAN,
     COMPARATOR_GREATER_THAN,
