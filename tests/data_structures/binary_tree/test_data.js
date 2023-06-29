72676ade6c6d4dfa68d198eb6df7ced038c0d540
Error-331 <red331@mail.ru>
2023-06-29 22:48:39 +0300

    Add some test data.

---
+++ tests/data_structures/binary_tree/test_data.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// external imports
+
+// internal imports
+
+// implementation
+const binarySearchTreeKeys1 = [40, 30, 50, 25, 35, 45, 60];
+const binarySearchTreeKeys2 = [8, 3, 10, 1, 6, 4, 7, 14, 13];
+const binarySearchTreeKeys3 = [11, 7, 15, 5, 3, 9, 8, 10, 13, 12, 14, 20, 18, 25];
+
+const unbalancedAVLBinarySearchTreeKeys1 = [3, 2, 1];
+const unbalancedAVLBinarySearchTreeKeys2 = [50, 30, 70, 10, 40, 5]
+
+// exports
+export {
+    binarySearchTreeKeys1,
+    binarySearchTreeKeys2,
+    binarySearchTreeKeys3,
+
+    unbalancedAVLBinarySearchTreeKeys1,
+    unbalancedAVLBinarySearchTreeKeys2,
+}
