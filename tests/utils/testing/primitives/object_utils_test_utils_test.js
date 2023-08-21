a7edd807727fe7a65521e1ed58eb22232d1aacf1
Error-331 <red331@mail.ru>
2023-08-21 21:39:56 +0300

    Add more tests for object utils test utils.

--- tests/utils/testing/primitives/object_utils_test_utils_test.js
+++ tests/utils/testing/primitives/object_utils_test_utils_test.js
@@ -216,6 +216,216 @@ test('Object utilities test utilities tests...', async (t) => {
             assert.throws(() => checkObjectsNotEqualByLinkDeep(testObject, testObj4));
         });
     });
+
+    await t.test('checkArraysNotEqualByLinkDeep() function tests...', async (t) => {
+        const testArray1 = [
+          1,
+          2,
+          3,
+        ];
+
+        const testArray2 = [
+          1,
+          2,
+          3,
+          [
+              'a',
+              'b',
+              'c',
+              [
+                'aa',
+                'bb',
+                'cc',
+              ],
+          ],
+        ];
+
+        const testArray3 = [
+            1,
+            2,
+            3,
+
+            {
+                testProp1: 'a',
+                testProp2: 'b',
+                testProp3: 'c',
+                testProp4: {
+                    testProp1: 'aa',
+                    testProp2: 'bb',
+                    testProp3: 'cc',
+                }
+            }
+        ];
+
+        const testArray4 = [
+            1,
+            {
+                testProp1: 1,
+                testProp2: 2,
+                testProp3: [
+                    'a',
+                    'b',
+                    {
+                        testProp1: 'aa',
+                        testProp2: {
+                            testProp1: '1',
+                            testProp2: '2',
+                            testProp3: [
+                                '1',
+                                '2'
+                            ],
+                            testProp4: [
+                                '11',
+                                '22',
+                            ]
+                        },
+                        testProp3: 'bb'
+                    }
+                ]
+            }
+        ];
+
+        await t.test('Should correctly compare two arrays and not throw error - case 1', () => {
+            const testArray = testArray1.slice();
+
+            checkArraysNotEqualByLinkDeep(testArray, testArray1);
+        });
+
+        await t.test('Should correctly compare two arrays and not throw error - case 2', () => {
+            const testArray = testArray2.slice();
+
+            testArray[3] = testArray2[3].slice();
+            testArray[3][3] = testArray2[3][3].slice();
+
+            checkArraysNotEqualByLinkDeep(testArray, testArray2);
+        });
+
+        await t.test('Should correctly compare two arrays and not throw error - case 3', () => {
+            const testArray = testArray3.slice();
+
+            testArray[3] = Object.assign({}, testArray3[3]);
+            testArray[3].testProp4 = Object.assign({}, testArray3[3].testProp4);
+
+            checkArraysNotEqualByLinkDeep(testArray, testArray3);
+        });
+
+        await t.test('Should correctly compare two arrays and not throw error - case 4', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+            testArray[1].testProp3 = testArray4[1].testProp3.slice();
+            testArray[1].testProp3[2] = Object.assign({}, testArray4[1].testProp3[2]);
+            testArray[1].testProp3[2].testProp2 = Object.assign({}, testArray4[1].testProp3[2].testProp2);
+            testArray[1].testProp3[2].testProp2.testProp3 = testArray4[1].testProp3[2].testProp2.testProp3.slice();
+            testArray[1].testProp3[2].testProp2.testProp4 = testArray4[1].testProp3[2].testProp2.testProp4.slice();
+
+            checkArraysNotEqualByLinkDeep(testArray, testArray4);
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 1', () => {
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray1, testArray1));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 2', () => {
+            const testArray = testArray2.slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray2));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 3', () => {
+            const testArray = testArray2.slice();
+
+            testArray[3] = testArray2[3].slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray2));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 4', () => {
+            const testArray = testArray2.slice();
+
+            testArray[3][3] = testArray2[3][3].slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray2));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 5', () => {
+            const testArray = testArray3.slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray3));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 6', () => {
+            const testArray = testArray3.slice();
+
+            testArray[3] = Object.assign({}, testArray3[3]);
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray3));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 7', () => {
+            const testArray = testArray3.slice();
+
+            testArray[3].testProp4 = Object.assign({}, testArray3[3].testProp4);
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray3));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 8', () => {
+            const testArray = testArray4.slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 9', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 10', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+            testArray[1].testProp3 = testArray4[1].testProp3.slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 11', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+            testArray[1].testProp3 = testArray4[1].testProp3.slice();
+            testArray[1].testProp3[2] = Object.assign({}, testArray4[1].testProp3[2]);
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 12', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+            testArray[1].testProp3 = testArray4[1].testProp3.slice();
+            testArray[1].testProp3[2] = Object.assign({}, testArray4[1].testProp3[2]);
+            testArray[1].testProp3[2].testProp2 = Object.assign({}, testArray4[1].testProp3[2].testProp2);
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+
+        await t.test('Should correctly compare two arrays and throw error - case 13', () => {
+            const testArray = testArray4.slice();
+
+            testArray[1] = Object.assign({}, testArray4[1]);
+            testArray[1].testProp3 = testArray4[1].testProp3.slice();
+            testArray[1].testProp3[2] = Object.assign({}, testArray4[1].testProp3[2]);
+            testArray[1].testProp3[2].testProp2 = Object.assign({}, testArray4[1].testProp3[2].testProp2);
+            testArray[1].testProp3[2].testProp2.testProp3 = testArray4[1].testProp3[2].testProp2.testProp3.slice();
+
+            assert.throws(() => checkArraysNotEqualByLinkDeep(testArray, testArray4));
+        });
+    });
 });
 
 // exports
