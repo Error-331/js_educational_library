74e5df09a9ae5ff5cbea993299ce8ec13aa81e09
Error-331 <red331@mail.ru>
2023-11-03 20:04:47 +0200

    Add basic insertion sort functions and tests for them.

---
+++ src/algorithms/sorting/insertion_sort.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// external imports
+
+// internal imports
+import { COMPARATOR_GREATER_THAN } from './../../constants/comparator_constants.js';
+
+import { isArray, isFunction } from './../../utils/misc/logic_utils.js';
+import { curry } from './../../utils/misc/functional_utils.js';
+import { defaultCompare } from './../../utils/misc/comparator_utils.js';
+
+// implementation
+function insertionSort(comparator, arrayToSort) {
+    if (!isArray(arrayToSort)) {
+        throw new Error('Cannot sort non-array entity');
+    }
+
+    if (!isFunction(comparator)) {
+        throw new Error('Cannot sort array - comparator is not a function');
+    }
+
+    const arrayToSortCopy = arrayToSort.slice();
+
+    for (let arrayIndex = 1; arrayIndex < arrayToSortCopy.length; arrayIndex++) {
+        let arraySubIndex = arrayIndex - 1;
+
+        let currentElement = arrayToSortCopy[arrayIndex];
+        let previousElement = arrayToSortCopy[arraySubIndex];
+
+        while(comparator(previousElement, currentElement) === COMPARATOR_GREATER_THAN && arraySubIndex >= 0) {
+            arrayToSortCopy[arraySubIndex + 1] = arrayToSortCopy[arraySubIndex];
+
+            arraySubIndex -= 1;
+            previousElement = arrayToSortCopy[arraySubIndex];
+        }
+
+        arrayToSortCopy[arraySubIndex + 1] = currentElement;
+    }
+
+    return arrayToSortCopy;
+}
+
+const insertionSortNumbers = curry(insertionSort)(defaultCompare);
+
+// exports
+export {
+    insertionSort,
+    insertionSortNumbers,
+}
---
+++ tests/algorithms/sorting/insertion_sort_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// external imports
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+// internal imports
+import { defaultCompare } from './../../../src/utils/misc/comparator_utils.js';
+import { insertionSort, insertionSortNumbers } from './../../../src/algorithms/sorting/insertion_sort.js';
+
+// implementation
+test('Insertion sort tests...', async (t) => {
+    const testIntegerArray1 = [5, 2, 1, 10, 3, 7, 0];
+    const testIntegerArray2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    const testIntegerArray3 = [0, 1, 2, 3, 4, 5];
+
+    await t.test('insertionSort() function tests...', async (t) => {
+        await t.test('Should correctly sort an array (iteger) - case 1', () => {
+            assert.deepStrictEqual(insertionSort(defaultCompare, testIntegerArray1), [0, 1, 2, 3, 5, 7, 10]);
+        });
+
+        await t.test('Should correctly sort an array (iteger) - case 2', () => {
+            assert.deepStrictEqual(insertionSort(defaultCompare, testIntegerArray2), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        });
+
+        await t.test('Should correctly sort an array (iteger) - case 3', () => {
+            assert.deepStrictEqual(insertionSort(defaultCompare, testIntegerArray3), [0, 1, 2, 3, 4, 5]);
+        });
+    });
+
+    await t.test('insertionSortNumbers() function tests...', async (t) => {
+        await t.test('Should correctly sort an array (iteger) - case 1', () => {
+            assert.deepStrictEqual(insertionSortNumbers(testIntegerArray1), [0, 1, 2, 3, 5, 7, 10]);
+        });
+
+        await t.test('Should correctly sort an array (iteger) - case 2', () => {
+            assert.deepStrictEqual(insertionSortNumbers(testIntegerArray2), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        });
+
+        await t.test('Should correctly sort an array (iteger) - case 3', () => {
+            assert.deepStrictEqual(insertionSortNumbers(testIntegerArray3), [0, 1, 2, 3, 4, 5]);
+        });
+    });
+});
+
+// exports
