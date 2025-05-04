cfd13cd187f7f54bee5c21ebe7f785820fcd7b12
Sergei <sselihov@brightsign.biz>
2025-05-04 20:04:45 +0300

    Minor fixes, add possibility to set setCookieOptions in setJWTResponseCookie() method.

--- src/security/authentication/strategies/abstract_authentication_strategy.ts
+++ src/security/authentication/strategies/abstract_authentication_strategy.ts
@@ -6,7 +6,7 @@
 abstract class AbstractAuthenticationStrategy {
     public abstract verifyUser(): Promise<boolean>;
 
-    public abstract signIn(...args: unknown): Promise<void>;
+    public abstract signIn(...args: unknown[]): Promise<void | unknown>;
     public abstract signOut(): Promise<void>;
 }
 
