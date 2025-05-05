c165a800286e207b1bbcb424876cd1a9751e8110
Sergei <sselihov@brightsign.biz>
2025-05-05 22:26:13 +0300

    Add AuthenticationVendor, AuthenticationProvider enums, UserAuthenticationStateInfo interface.

--- src/declarations/security/authentication_declarations.ts
+++ src/declarations/security/authentication_declarations.ts
@@ -3,19 +3,40 @@
 // internal imports
 
 // implementation
+enum AuthenticationVendor {
+    Firebase = 'Firebase',
+    Unknown = 'Unknown',
+}
+
+enum AuthenticationProvider {
+    Anonymous = 'Anonymous',
+    Unknown = 'Unknown',
+}
+
 type EmailPasswordSingInParams = [email: string, password: string];
 
 interface AuthenticationSignInStrategy<SignInParams> {
-    signIn(...args: SignInParams): Promise<void>;
+    signIn(...args: SignInParams[]): Promise<void>;
     signOut(): Promise<void>;
 }
 
 interface AuthenticationEmailPasswordSignInStrategy extends AuthenticationSignInStrategy<EmailPasswordSingInParams> {}
 
+interface UserAuthenticationStateInfo {
+    authenticated: boolean;
+    vendor?: AuthenticationVendor;
+    provider?: AuthenticationProvider;
+}
+
 // exports
 export {
+    AuthenticationVendor,
+    AuthenticationProvider,
+
     EmailPasswordSingInParams,
 
     AuthenticationSignInStrategy,
     AuthenticationEmailPasswordSignInStrategy,
+
+    UserAuthenticationStateInfo,
 }
