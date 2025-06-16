619264d745e4693ea30c2305698aee37f67af938
Sergei <sselihov@brightsign.biz>
2025-06-16 03:04:31 +0300

    Add combineURLPaths() function, minor fixes.

--- src/utils/net/uri_utils.ts
+++ src/utils/net/uri_utils.ts
@@ -1,18 +1,34 @@
 // external imports
 
 // internal imports
-import { isArray } from '../misc/logic_utils';
+import { isArray, isString } from '../misc/logic_utils';
 
 // implementation
 function composeURLPath(pathParts: (string | number)[]): string {
     if (!isArray(pathParts)) {
-        throw new RangeError('The provided path parts are not in array form - cannot compose URL path');
+        throw new RangeError('cannot compose URL path - the provided path parts are not in array form');
     }
 
     return pathParts.join('/');
 }
 
+function combineURLPaths(pathPart1: string, pathPart2: string): string {
+    if (!isString(pathPart1)) {
+        throw new RangeError('Cannot combine URL parts - first path part must be of type string');
+    }
+
+    if (!isString(pathPart2)) {
+        throw new RangeError('Cannot combine URL parts - second path part must be of type string');
+    }
+
+    const sanitizedPathPart1 = pathPart1.endsWith('/') ? pathPart1.slice(0, -1) : pathPart1;
+    const sanitizedPathPart2 = pathPart2.startsWith('/') ? pathPart2.slice(1) : pathPart2;
+
+    return `${sanitizedPathPart1}/${sanitizedPathPart2}`;
+}
+
 // exports
 export {
     composeURLPath,
+    combineURLPaths,
 }
