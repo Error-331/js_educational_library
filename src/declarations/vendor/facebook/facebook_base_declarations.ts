cf6634fee71b9b6740d31ec6fbe5e3d700ce61f6
Sergei <sselihov@brightsign.biz>
2025-06-22 23:09:19 +0300

    Add Facebook common constants, add Facebook oauth api declarations, add Facebook base declarations, minor fixes.

---
+++ src/declarations/vendor/facebook/facebook_base_declarations.ts
@@ -0,0 +1,16 @@
+// external imports
+
+// internal imports
+
+// implementation
+type FacebookErrorResponse = {
+    message: string;
+    type: string;
+    code: number;
+    fbtrace_id?: string
+}
+
+// exports
+export {
+    FacebookErrorResponse,
+}
---
+++ src/declarations/vendor/facebook/facebook_oauth_api_declarations.ts
@@ -0,0 +1,14 @@
+// external imports
+
+// internal imports
+
+// implementation
+type FacebookOAuthAccessTokenResponse = {
+    access_token: string;
+    token_type: 'bearer';
+}
+
+// exports
+export {
+    FacebookOAuthAccessTokenResponse,
+}
