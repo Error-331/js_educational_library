6dbabbcfa0002fbca26851ce7ffc449c4238a784
Sergei <sselihov@brightsign.biz>
2025-06-22 21:10:30 +0300

    Add combineMultipleURLPaths() and sanitizeURLPathPart(), minor fixes to combineURLPaths

--- src/utils/net/uri_utils.ts
+++ src/utils/net/uri_utils.ts
@@ -12,6 +12,17 @@ function composeURLPath(pathParts: (string | number)[]): string {
     return pathParts.join('/');
 }
 
+function sanitizeURLPathPart(urlPathPart: string) {
+    if (!isString(urlPathPart)) {
+        throw new RangeError('Cannot sanitize URL path part - url path part must be of type string');
+    }
+
+    urlPathPart = urlPathPart.endsWith('/') ? urlPathPart.slice(0, -1) : urlPathPart;
+    urlPathPart = urlPathPart.startsWith('/') ? urlPathPart.slice(1) : urlPathPart;
+
+    return urlPathPart;
+}
+
 function combineURLPaths(pathPart1: string, pathPart2: string): string {
     if (!isString(pathPart1)) {
         throw new RangeError('Cannot combine URL parts - first path part must be of type string');
@@ -21,14 +32,27 @@ function combineURLPaths(pathPart1: string, pathPart2: string): string {
         throw new RangeError('Cannot combine URL parts - second path part must be of type string');
     }
 
-    const sanitizedPathPart1 = pathPart1.endsWith('/') ? pathPart1.slice(0, -1) : pathPart1;
-    const sanitizedPathPart2 = pathPart2.startsWith('/') ? pathPart2.slice(1) : pathPart2;
+    const sanitizedPathPart1 = sanitizeURLPathPart(pathPart1);
+    const sanitizedPathPart2 = sanitizeURLPathPart(pathPart2)
 
     return `${sanitizedPathPart1}/${sanitizedPathPart2}`;
 }
 
+function combineMultipleURLPaths(pathParts: string[]): string {
+    if (!isArray(pathParts) || pathParts.some(part => !isString(part))) {
+        throw new RangeError('Cannot combine multiple URL parts - all path parts must be strings');
+    }
+
+    const sanitizedParts = pathParts.map(sanitizeURLPathPart);
+    return sanitizedParts.join('/');
+}
+
 // exports
 export {
+    sanitizeURLPathPart,
+
     composeURLPath,
     combineURLPaths,
+
+    combineMultipleURLPaths,
 }
