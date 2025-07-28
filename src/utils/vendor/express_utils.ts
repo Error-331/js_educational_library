cf8f1b23985492668b616510606deeba0a0bf1d0
Sergei <sselihov@brightsign.biz>
2025-07-28 23:08:09 +0300

    Add express js utils.

---
+++ src/utils/vendor/express_utils.ts
@@ -0,0 +1,32 @@
+// external imports
+import { Request, Response } from 'express';
+
+// internal imports
+import {CommonServerReturn} from '../../declarations/net/api/common_declarations';
+import BaseHTTPServerController from '../../net/api/common/base_http_server_controller';
+
+// implementation
+function handleJSONOnly(handler: (req: Request, res: Response) => CommonServerReturn, req: Request, res: Response): void {
+    res.format({
+        async html() {
+            return BaseHTTPServerController.serverNotAcceptable406Error(req, res);
+        },
+
+        async text() {
+            return BaseHTTPServerController.serverNotAcceptable406Error(req, res);
+        },
+
+        async json() {
+            return handler(req, res);
+        },
+
+        async default() {
+            return BaseHTTPServerController.serverNotAcceptable406Error(req, res);
+        }
+    });
+}
+
+// exports
+export {
+    handleJSONOnly,
+}
