0485f636a290ae616aa7bb9ee7160367286fb81d
Sergei <sselihov@brightsign.biz>
2025-07-28 03:48:34 +0300

    Add vendor constants.

---
+++ src/constants/vendor_constants.ts
@@ -0,0 +1,14 @@
+// external imports
+
+// internal imports
+
+// implementation
+enum ServerFrameworkVendorName {
+    ExpressJS = 'ExpressJS',
+    NextJS = 'NextJS',
+}
+
+// exports
+export {
+    ServerFrameworkVendorName,
+}
