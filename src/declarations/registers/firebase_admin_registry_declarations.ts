0c493fd53fe78238c7af270b052ea20206fff260
error-442 <sergei.error.331@gmail.com>
2025-08-04 03:48:15 +0300

    Aff plain text encryptor/decryptor (and constant for it), add some doc regarding Firebase emulators, add Firebase admin registry declarations.

---
+++ src/declarations/registers/firebase_admin_registry_declarations.ts
@@ -0,0 +1,12 @@
+// external imports
+import { Credential } from "firebase-admin/app";
+
+// internal imports
+
+// implementation
+type WrappedCredential = { credential: Credential };
+
+// exports
+export {
+    WrappedCredential,
+}
