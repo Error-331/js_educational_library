1c3f0140d541aa7131d4f67259099b5507d906b4
Sergei <sselihov@brightsign.biz>
2025-09-08 03:34:32 +0300

    Add calcFileSizeInBytesAsync(), minor fixes.

--- src/utils/file/general_file_utils.ts
+++ src/utils/file/general_file_utils.ts
@@ -1,6 +1,8 @@
 // external imports
+import { stat } from 'node:fs/promises';
 
 // internal imports
+import { isString } from '../misc/logic_utils';
 
 // implementation
 function isFile(file: File): file is File {
@@ -9,15 +11,25 @@ function isFile(file: File): file is File {
 
 async function extractBinaryDataFromFile(file: File): Promise<Uint8Array> {
     if (!isFile(file)) {
-        throw new RangeError('Cannot extract binary data from a file - provided value is not a valid file')
+        throw new RangeError('Cannot extract binary data from a file - provided value is not a valid file');
     }
 
     const arrayBuffer = await file.arrayBuffer();
     return new Uint8Array(arrayBuffer);
 }
 
+async function calcFileSizeInBytesAsync(pathToFile: string): Promise<number> {
+    if (!isString(pathToFile)) {
+        throw new RangeError('Cannot calculate size of the file in bytes - provided path to file is not a string');
+    }
+
+    const stats = await stat(pathToFile);
+    return stats.size;
+}
+
 // exports
 export {
     isFile,
     extractBinaryDataFromFile,
+    calcFileSizeInBytesAsync,
 }
