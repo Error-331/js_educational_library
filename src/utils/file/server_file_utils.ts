8c728e7d69c4be3ee5261eebf02fcd366846dab3
Sergei <sselihov@brightsign.biz>
2025-09-29 13:11:12 +0300

    Add replaceFileNameWithRandomUUID() function.

--- src/utils/file/server_file_utils.ts
+++ src/utils/file/server_file_utils.ts
@@ -2,6 +2,8 @@
 import { stat } from 'node:fs/promises';
 
 // internal imports
+import { generateAlmostRandomUUID } from '../primitives/string/random_string_generation_utils';
+import { extractFileExtension } from '../misc/path_utils';
 import { isString } from '../misc/logic_utils';
 
 // implementation
@@ -14,7 +16,17 @@ async function calcFileSizeInBytesAsync(pathToFile: string): Promise<number> {
     return stats.size;
 }
 
+function replaceFileNameWithRandomUUID(filename: string): string {
+    if (!isString(filename)) {
+        throw new RangeError('Cannot replace filename with UUID - provided file name is not a string');
+    }
+
+    const fileExt = extractFileExtension(filename);
+    return `${generateAlmostRandomUUID()}${fileExt}`;
+}
+
 // exports
 export {
     calcFileSizeInBytesAsync,
+    replaceFileNameWithRandomUUID,
 }
