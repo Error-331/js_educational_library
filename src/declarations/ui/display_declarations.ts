65d228db01387f7583c9473a39474dac4854dee1
error-442 <sergei.error.331@gmail.com>
2026-08-09 16:42:57 +0300

    Add display declarations.

---
+++ src/declarations/ui/display_declarations.ts
@@ -0,0 +1,15 @@
+// external imports
+
+// internal imports
+
+// implementation
+enum DisplayOrientation {
+    Landscape = 'Landscape',
+    Portrait = 'Portrait',
+    Square = 'Square',
+}
+
+// exports
+export {
+    DisplayOrientation,
+}
