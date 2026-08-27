3bafad90b6eababe098c86bc7cd55db2c73cf568
Sergei <sergei.error.331@gmail.com>
2026-08-27 05:00:53 +0300

    Add Firebase hosting constants.

---
+++ src/constants/net/api/firebase/firebase_hosting_constants.ts
@@ -0,0 +1,11 @@
+// external imports
+
+// internal imports
+
+// implementation
+const FIREBASE_HOSTING_FREE_TIER_MAX_STORAGE = 10737418240; // bytes
+
+// exports
+export {
+    FIREBASE_HOSTING_FREE_TIER_MAX_STORAGE,
+}
