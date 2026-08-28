30586a10cc3766953d1de35986720346e6e38c50
Sergei <sergei.error.331@gmail.com>
2026-08-28 16:52:30 +0300

    Add more GCP monitoring constants.

---
+++ src/constants/net/api/gcp/gcp_monitoring/gcp_monitoring_generative_language_constants.ts
@@ -0,0 +1,11 @@
+// external imports
+
+// internal imports
+
+// implementation
+const GCP_MONITORING_GENERATIVE_LANGUAGE_METRIC = 'generativelanguage.googleapis.com/generate_content_usage_output_token_count';
+
+// exports
+export {
+    GCP_MONITORING_GENERATIVE_LANGUAGE_METRIC,
+}
---
+++ src/constants/net/api/gcp/gcp_monitoring/gcp_monitoring_storage_constants.ts
@@ -0,0 +1,13 @@
+// external imports
+
+// internal imports
+
+// implementation
+const GCP_MONITORING_STORAGE_STORAGE_TOTAL_BYTES_METRIC = 'storage.googleapis.com/storage/total_bytes';
+const GCP_MONITORING_STORAGE_STORAGE_TOTAL_BYTES_METRIC_PERIOD = 300 + 690;
+
+// exports
+export {
+    GCP_MONITORING_STORAGE_STORAGE_TOTAL_BYTES_METRIC,
+    GCP_MONITORING_STORAGE_STORAGE_TOTAL_BYTES_METRIC_PERIOD,
+}
