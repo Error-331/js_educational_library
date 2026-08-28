cb2a814c7a62448aae7db3d5887b57ea0d5ee327
Sergei <sergei.error.331@gmail.com>
2026-08-28 17:03:27 +0300

    Minor fix.

--- src/constants/net/api/gcp/gcp_monitoring/gcp_monitoring_generative_language_constants.ts
+++ src/constants/net/api/gcp/gcp_monitoring/gcp_monitoring_generative_language_constants.ts
@@ -3,9 +3,9 @@
 // internal imports
 
 // implementation
-const GCP_MONITORING_GENERATIVE_LANGUAGE_METRIC = 'generativelanguage.googleapis.com/generate_content_usage_output_token_count';
+const GCP_MONITORING_GENERATIVE_LANGUAGE_GENERATE_CONTENT_USAGE_OUTPUT_TOKEN_COUNT_METRIC = 'generativelanguage.googleapis.com/generate_content_usage_output_token_count';
 
 // exports
 export {
-    GCP_MONITORING_GENERATIVE_LANGUAGE_METRIC,
+    GCP_MONITORING_GENERATIVE_LANGUAGE_GENERATE_CONTENT_USAGE_OUTPUT_TOKEN_COUNT_METRIC,
 }
